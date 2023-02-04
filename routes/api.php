0816a5c3d3d955fdc10455b92989829d20162811
Kaiji21 <elhaiba120@gmail.com>
2023-02-05 00:42:14 +0100

    Modified .env file to update project configuration, including database connection details

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CentreController;
+
 
 /*
 |--------------------------------------------------------------------------
