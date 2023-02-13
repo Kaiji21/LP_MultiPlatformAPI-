f770e0a0a25574cdb907023643790c5b05f6a152
Kaiji21 <elhaiba120@gmail.com>
2023-02-13 14:13:23 +0100

    Modified PublicationController to encode image from database to base64 for proper representation in Mobile app

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -8,15 +8,17 @@ class PublicationController extends Controller
 {
     public function Get_Publication(){
          try{
-        $Publication = DB::table('publication')
+        $Publications = DB::table('publication')
         ->join('employe','employe.idEmploye','=','publication.idEmploye')
-        ->leftJoin('image_publication','image_publication.id_Publication','=',"publication.idEmploye")
-        ->join('utilisateur','utilisateur.id_utlisateur','=','employe.id_utlisateur')
+        ->leftJoin('image_publication','image_publication.id_Publication','=',"publication.id_publication")
         ->select('employe.nomEmploye','publication.titre_publication','publication.contenu_publication','image_publication.photo')
         ->get();
+        foreach ($Publications as &$Publication) {
+            $Publication->photo = base64_encode($Publication->photo);
+        }
         return response()->json([
             'status'=>200,
-            'Publications'=>$Publication
+            'Publications'=>$Publications
 
         ]);
 
