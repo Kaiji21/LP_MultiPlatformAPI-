3cf4d6de23feb9c87da9363f67b5d0612791bdd6
Kaiji21 <elhaiba120@gmail.com>
2023-02-13 14:46:37 +0100

    Updated the Get_Publication function to include the publication date

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -11,7 +11,7 @@ class PublicationController extends Controller
         $Publications = DB::table('publication')
         ->join('employe','employe.idEmploye','=','publication.idEmploye')
         ->leftJoin('image_publication','image_publication.id_Publication','=',"publication.id_publication")
-        ->select('employe.nomEmploye','publication.titre_publication','publication.contenu_publication','image_publication.photo')
+        ->select('employe.nomEmploye','publication.titre_publication','publication.contenu_publication','publication.date_publication','image_publication.photo')
         ->get();
         foreach ($Publications as &$Publication) {
             $Publication->photo = base64_encode($Publication->photo);
